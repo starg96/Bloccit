0f488c5b3de9420013ecb0fb210caa4ff325e7cb
Kyle <starg96@gmail.com>
2015-04-08 18:05:37 -0500

    Complete static pages assignment

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/contacts'
+
+  get 'welcome/index'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+  
     root to: 'welcome#index'
 end
