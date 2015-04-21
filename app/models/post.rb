9c4b2bcadb5143ebeec47b345d630e8ba3dc488e
Kyle <starg96@gmail.com>
2015-04-21 17:55:57 -0500

    Complete Associations assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,8 @@ class Post < ActiveRecord::Base
 	belongs_to :user
 
 	default_scope {order('created_at DESC') }
+
+	scope :ordered_by_title, -> { reorder(title: :asc) }
+	scope :ordered_by_reverse_created_at, -> { reorder(:created_at).reverse_order }
+
 end
