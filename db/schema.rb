e7f308fcc2d15f7a2d18a1f33a1d74ae03f0753d
Kyle <starg96@gmail.com>
2015-05-03 11:47:28 -0500

    Halfway through

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150424225016) do
+ActiveRecord::Schema.define(version: 20150502164819) do
 
   create_table "advertisments", force: :cascade do |t|
     t.string   "title"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 20150424225016) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "summaries", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public"
@@ -77,6 +84,7 @@ ActiveRecord::Schema.define(version: 20150424225016) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
