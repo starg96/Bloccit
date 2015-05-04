cf5e4d2c78985c8476331044200bf60913407dbb
Kyle <starg96@gmail.com>
2015-05-03 19:26:52 -0500

    Completed uploading images assignment

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:topic).permit(:title, :body)
+    params.require(:post).permit(:title, :body, :image)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,9 @@ class Post < ActiveRecord::Base
 	has_many :comments
 	belongs_to :user
 	belongs_to :topic
+#	belongs_to :image
+
+	mount_uploader :image, ImageUploader
 
 	default_scope {order('created_at DESC') }
 
