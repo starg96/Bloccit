f2c53703c3a307aea23227ea39776d55740882f9
Kyle <starg96@gmail.com>
2015-05-06 16:56:27 -0500

    complete rspec assignment

---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,9 @@
+descibe Vote do
+	describe "validation" do
+		describe "value validation" do
+			it "only allows -1 or 1 as values" do
+				expect( value validation ).to eq(1) || expect( value validation ).to eq(-1)
+			end
+		end
+	end
+end
