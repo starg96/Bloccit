d2c0c7b577b84bcf2d5ee7612aaac7a778ad31d2
Kyle <starg96@gmail.com>
2015-05-06 15:58:35 -0500

    Finished assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,8 +3,6 @@ class CommentsController < ApplicationController
     @comment =	Comment.new(post_id: params[:post_id], body: params[:comment][:body], user: current_user )
     @post = @comment.post
     authorize @comment
-    # We want a comment attached to a post and a user when we are done here.
-    # @comment = current_user.comments.build(params.require(:comment).permit(:title, :body))
 
     if @comment.save
       flash[:notice] = "comment was saved."
@@ -16,17 +14,17 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
+
+    @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
 
     authorize @comment
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
+      redirect_to [@comment.post.topic, @comment.post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
+      redirect_to [@comment.post.topic, @comment.post]
     end
   end
 end
