4441ca970ccfbb0e8fff3a6026c2b2bde7503f71
Kyle <starg96@gmail.com>
2015-05-11 15:46:47 -0500

    finished up voting checkpoint

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,36 @@
 class Post < ActiveRecord::Base
 	has_one :summary
 	has_many :comments, dependent: :destroy
+  has_many :votes, dependent: :destroy
 	belongs_to :user
 	belongs_to :topic
 
-	default_scope {order('created_at DESC') }
+	default_scope {order('rank DESC') }
 
 	validates :title, length: { minimum: 5 }, presence: true
 	validates :body, length: { minimum: 20}, presence: true
-	validates :topic, presence: true
-	validates :user, presence: true
+#	validates :topic, presence: true
+#	validates :user, presence: true
+
+
+   	def up_votes
+    	votes.where(value: 1).count
+    	#both of these methods are going to output a positive number
+   	end
+
+   	def down_votes
+   		votes.where(value: -1).count
+   		#both of these methods are going to output a positive number
+   	end
+
+   	def points
+   		votes.sum(:value)
+   	end
+
+    def update_rank
+      age_in_days = (created_at - Time.new(1970,1,1)) / (60 * 60 *24) # 1 day in seconds
+      new_rank = points + age_in_days
+
+      update_attribute(:rank, new_rank)
+    end
 end
