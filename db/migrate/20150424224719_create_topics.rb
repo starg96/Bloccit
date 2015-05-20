18f2502ce5d33f759d0906656c57e86ff18ba979
Kyle <starg96@gmail.com>
2015-05-20 17:26:23 -0500

    finsihed private topics assignment

--- db/migrate/20150424224719_create_topics.rb
+++ db/migrate/20150424224719_create_topics.rb
@@ -2,7 +2,7 @@ class CreateTopics < ActiveRecord::Migration
   def change
     create_table :topics do |t|
       t.string :name
-      t.boolean :public, defult: true
+      t.boolean :public, default: true
       t.text :description
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -64,10 +64,10 @@ ActiveRecord::Schema.define(version: 20150515185445) do
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
-    t.boolean  "public"
+    t.boolean  "public",      default: true
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   create_table "users", force: :cascade do |t|
