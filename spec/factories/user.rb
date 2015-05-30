674d1280032a1c4649e6e2c5193a1811a4db0f24
Kyle <starg96@gmail.com>
2015-05-30 15:08:00 -0500

    almost done

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -5,5 +5,20 @@ FactoryGirl.define do
 		password "helloworld"
 		password_confirmation "helloworld"
 		confirmed_at Time.now
+
+		factory :user_with_post_and_comment do
+			title "Post Title"
+			body "Post bodies must be pretty long."
+			user
+			topic { Topic.create(name: 'Topic name') }
+
+			body "This is a new comment."
+			user
+			post
+
+			after(:build) do |comment|
+				comment.class.skip_callback(:create, :after, :send_favorite_emails)
+			end
+		end
 	end
 end
